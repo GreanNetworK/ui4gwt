f72b6eaa705a447efd7eb4f417e9ff769de19fbb
wjirawong <wjirawong@shipco.com>
2013-07-23 09:47:45 +0700

    add constructor for SlideEvent

--- ui/src/main/java/com/mockupcode/ui4gwt/ui/slider/event/SlideEvent.java
+++ ui/src/main/java/com/mockupcode/ui4gwt/ui/slider/event/SlideEvent.java
@@ -1,5 +1,8 @@
 package com.mockupcode.ui4gwt.ui.slider.event;
 
+import com.google.gwt.user.client.Event;
+import com.mockupcode.ui4gwt.ui.slider.Slider;
+
 /**
  * Created with IntelliJ IDEA.
  * User: San
@@ -8,4 +11,8 @@ package com.mockupcode.ui4gwt.ui.slider.event;
  * To change this template use File | Settings | File Templates.
  */
 public class SlideEvent extends BaseEvent {
+	
+	public SlideEvent(Slider source, Event event,Double[] values){
+		super(source, event, values);
+	}
 }
