6cb44ab0235168c3bf604fc0c261ef115cae2ea4
wjirawong <wjirawong@shipco.com>
2013-08-05 11:30:39 +0700

    add get/set for sliderOptions and test it.

--- ui/src/main/java/com/mockupcode/ui4gwt/ui/slider/options/SliderOptions.java
+++ ui/src/main/java/com/mockupcode/ui4gwt/ui/slider/options/SliderOptions.java
@@ -2,41 +2,70 @@ package com.mockupcode.ui4gwt.ui.slider.options;
 
 
 public class SliderOptions {
-
-	public Boolean getAnimate() {
-		return false;		
+	
+	private boolean animate = false;
+	private boolean disabled = false;
+	private int max = 100;
+	private int min = 0;
+	private Orientation orientation = Orientation.HORIZONTAL;
+	private boolean range = false;
+	private int step = 1;
+	private Number value = 0;
+	private Number[] values = null;
+	
+	public boolean isAnimate() {
+		return animate;
 	}
-
-	public Boolean getDisabled() {
-		return false;
+	public void setAnimate(boolean animate) {
+		this.animate = animate;
 	}
-
-	public Integer getMax() {
-		return 100;
+	public boolean isDisabled() {
+		return disabled;
 	}
-
-	public Integer getMin() {
-		return 0;
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
 	}
-
+	public int getMax() {
+		return max;
+	}
+	public void setMax(int max) {
+		this.max = max;
+	}
+	public int getMin() {
+		return min;
+	}
+	public void setMin(int min) {
+		this.min = min;
+	}
 	public Orientation getOrientation() {
-		return Orientation.HORIZONTAL;
+		return orientation;
 	}
-
-	public Boolean getRange() {
-		return false;
+	public void setOrientation(Orientation orientation) {
+		this.orientation = orientation;
 	}
-
-	public Integer getStep() {
-		return 1;
+	public boolean isRange() {
+		return range;
 	}
-
+	public void setRange(boolean range) {
+		this.range = range;
+	}
+	public int getStep() {
+		return step;
+	}
+	public void setStep(int step) {
+		this.step = step;
+	}
 	public Number getValue() {
-		return 0;
+		return value;
 	}
-
+	public void setValue(Number value) {
+		this.value = value;
+	}
 	public Number[] getValues() {
-		return null;
+		return values;
+	}
+	public void setValues(Number[] values) {
+		this.values = values;
 	}
 
 }
--- ui/src/test/java/com/mockupcode/ui4gwt/ui/slider/options/SliderOptionsTest.java
+++ ui/src/test/java/com/mockupcode/ui4gwt/ui/slider/options/SliderOptionsTest.java
@@ -1,29 +1,42 @@
 package com.mockupcode.ui4gwt.ui.slider.options;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SliderOptionsTest {
 	
-	private static SliderOptions options;
+	private SliderOptions options;
 	
-	@BeforeClass
-	public static void setUp(){
+	@Before
+	public void setUp(){
 		options = new SliderOptions();
 	}
 
 	@Test
 	public void testDefaultOptionsAnimateValue() {
-		assertThat(options.getAnimate(), is(false));
+		assertThat(options.isAnimate(), is(false));
+	}
+	
+	@Test
+	public void testSetAndGetAnimateValue(){
+		options.setAnimate(true);
+		assertThat(options.isAnimate(), is(true));
 	}
 	
 	@Test
 	public void testDefaultOptionsDisabledValue(){
-		assertThat(options.getDisabled(), is(false));
+		assertThat(options.isDisabled(), is(false));
+	}
+	
+	@Test
+	public void testSetAndGetDisabledValue(){
+		options.setDisabled(true);
+		assertThat(options.isDisabled(), is(true));
 	}
 	
 	@Test
@@ -32,8 +45,20 @@ public class SliderOptionsTest {
 	}
 	
 	@Test
+	public void testSetAndGetMaxValue(){
+		options.setMax(50);
+		assertThat(options.getMax(), is(50));
+	}
+	
+	@Test
 	public void testDefaultOptionsMinValue(){
 		assertThat(options.getMin(), is(0));
+	}
+	
+	@Test
+	public void  testSetAndGetMinValue(){
+		options.setMin(50);
+		assertThat(options.getMin(), is(50));
 	}
 	
 	@Test
@@ -42,8 +67,20 @@ public class SliderOptionsTest {
 	}
 	
 	@Test
+	public void testSetAndGetOrientationValue(){
+		options.setOrientation(Orientation.VERTICAL);
+		assertThat(options.getOrientation(), equalTo(Orientation.VERTICAL));
+	}
+	
+	@Test
 	public void testDefaultOptionsRangeValue(){
-		assertThat(options.getRange(), is(false));
+		assertThat(options.isRange(), is(false));
+	}
+	
+	@Test
+	public void testSetAndGetRangeValue(){
+		options.setRange(true);
+		assertThat(options.isRange(), is(true));
 	}
 	
 	@Test
@@ -52,13 +89,32 @@ public class SliderOptionsTest {
 	}
 	
 	@Test
+	public void testSetAndGetStepValue(){
+		options.setStep(5);
+		assertThat(options.getStep(), is(5));
+	}
+	
+	@Test
 	public void testDefaultOptionsValue(){
 		assertThat(options.getValue(), is((Number)0));
 	}
 	
 	@Test
-	public void tstDefaultOptionsValues(){
+	public void testSetAndGetValue(){
+		options.setValue(10);
+		assertThat(options.getValue(), equalTo((Number)10));
+	}
+	
+	@Test
+	public void testDefaultOptionsValues(){
 		assertThat(options.getValues(), nullValue());
+	}
+	
+	@Test
+	public void testSetAndGetValues(){
+		Number[] values = new Number[]{10,20};
+		options.setValues(values);
+		assertThat(options.getValues(), is(new Number[]{10,20}));
 	}
 
 }
